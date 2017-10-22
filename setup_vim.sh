d51ac4bbc6ebfd0fab1ca57395096cf7a11b81ba
Michael Horowitz <michael@horowitz.name>
2017-10-21 22:47:03 -0400

    Fixed broken ln command for vimrc and switched to symbolic link.

--- setup_vim.sh
+++ setup_vim.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./vimrc ~/.vim/vimrc
-ln ~/.vimrc ~/.vim/vimrc
+ln -s ~/.vim/vimrc ~/.vimrc
 vim +PluginInstall +qall
 
 echo "dont forget to install / update YoucompleteMe"
